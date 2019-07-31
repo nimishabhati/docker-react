bb95dca06e2626a6b11941d1f7898b18f942ee54
Nimisha Bhati <nimishabhati@Nimishas-MacBook-Air.local>
2019-07-30 23:12:01 -0700

    Updated text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nimisha is cute girl is girl
+          Arish and Nimisha are cute
         </p>
         <a
           className="App-link"
